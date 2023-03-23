ac31150f73cdcebe1a8ec679ec159b7edf4993ff
Daniel Di Giovanni <dannyjdigio@gmail.com>
2023-03-22 23:11:14 -0400

    VEA-8 Added customer service and controller.

---
+++ src/main/java/com/danieldigiovanni/email/customer/CustomerController.java
@@ -0,0 +1,14 @@
+package com.danieldigiovanni.email.customer;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+}
