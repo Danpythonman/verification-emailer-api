5ed0e068d00f02aa88bbc3852d597fd418a77b57
Daniel Di Giovanni <dannyjdigio@gmail.com>
2023-03-22 23:50:11 -0400

    VEA-8 Added data constraints and exception handler
    Added constraints for the customer entity (not null, email) and added an
    exception handler to return a 400 status response for these exceptions.

--- src/main/java/com/danieldigiovanni/email/customer/Customer.java
+++ src/main/java/com/danieldigiovanni/email/customer/Customer.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.Date;
 
@@ -13,12 +15,14 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
     private String name;
+    @NotNull
+    @Email
     private String email;
     private Date createdAt;
 
-    public Customer() {
-    }
+    public Customer() { }
 
     public Long getId() {
         return id;
--- src/main/java/com/danieldigiovanni/email/customer/CustomerController.java
+++ src/main/java/com/danieldigiovanni/email/customer/CustomerController.java
@@ -1,10 +1,18 @@
 package com.danieldigiovanni.email.customer;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class CustomerController {
@@ -23,6 +31,20 @@ public class CustomerController {
     @GetMapping("/customer/{id}")
     public Customer getCustomerById(@PathVariable("id") Long id) {
         return this.customerService.getCustomerById(id);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public Map<String, String> handleValidationExceptions(
+        ConstraintViolationException exception
+    ) {
+        return exception.getConstraintViolations()
+            .stream()
+            .collect(Collectors.toMap(
+                constraintViolation -> constraintViolation.getPropertyPath()
+                    .toString(),
+                ConstraintViolation::getMessage
+            ));
     }
 
 }
