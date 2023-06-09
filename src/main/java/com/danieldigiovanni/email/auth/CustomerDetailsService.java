72f8749d9d12ca1a4b2d8cf943c6dbba63f5bf1d
Daniel Di Giovanni <dannyjdigio@gmail.com>
2023-06-09 16:48:35 -0400

    VEA-11 Customer details service.
    Added implementations for UserDetails and UserDetailsService
    (CustomerDetails and CustomerDetailsService). This will be used in
    authentication.

---
+++ src/main/java/com/danieldigiovanni/email/auth/CustomerDetailsService.java
@@ -0,0 +1,34 @@
+package com.danieldigiovanni.email.auth;
+
+import com.danieldigiovanni.email.customer.Customer;
+import com.danieldigiovanni.email.customer.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerDetailsService implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerDetailsService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public CustomerDetails loadUserByUsername(String username) throws UsernameNotFoundException, NumberFormatException {
+        Long customerId = Long.valueOf(username);
+
+        Customer customer = this.customerRepository.findById(customerId)
+            .orElseThrow(() ->
+                new UsernameNotFoundException("Customer not found")
+            );
+
+        return new CustomerDetails(
+            customer.getEmail(), customer.getPassword()
+        );
+    }
+
+}
